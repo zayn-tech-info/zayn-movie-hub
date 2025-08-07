234c28683bdb5b1b78fd2d73ee76ff244ce6d061
zayn-tech-info <zayntech.info@gmail.com>
2025-08-06 22:17:27 -0300

    First commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <p className="font-bold text-xl">
+          Hello <span>Zayn</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
