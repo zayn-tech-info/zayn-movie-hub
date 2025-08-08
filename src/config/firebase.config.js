a1c0c846cc9c3486a8ddda154f24c2d76ffd923b
zayn-tech-info <zayntech.info@gmail.com>
2025-08-08 22:47:22 +0100

    added firebase code

---
+++ src/config/firebase.config.js
@@ -0,0 +1,13 @@
+ import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5TAdHYEOSM0YgGMD0kRJSojHSrkdTq30",
+  authDomain: "movie-hub-9075b.firebaseapp.com",
+  projectId: "movie-hub-9075b",
+  storageBucket: "movie-hub-9075b.firebasestorage.app",
+  messagingSenderId: "836155428711",
+  appId: "1:836155428711:web:97e2f617366db37220e9c2",
+  measurementId: "G-71M2ZPY9NF",
+};
+
+const app = initializeApp(firebaseConfig);
